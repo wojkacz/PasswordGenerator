5de66f95e6311867e012b4a07abf465612a6bcf9
wojkacz <252813@student.pwr.edu.pl>
2021-09-30 01:26:48 +0200

    UI created

--- src/main/java/pl/wojkacz/Launcher.java
+++ src/main/java/pl/wojkacz/Launcher.java
@@ -1,7 +1,9 @@
 package pl.wojkacz;
 
 import javafx.application.Application;
+
 import javafx.fxml.FXMLLoader;
+
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -14,8 +16,10 @@ public class Launcher extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         URL url = new File("src/main/java/pl/wojkacz/App.fxml").toURI().toURL();
+
         Parent root = FXMLLoader.load(url);
         Scene scene = new Scene(root, 1024, 576);
+
         stage.setScene(scene);
         stage.setTitle("Password Generator");
         stage.show();
